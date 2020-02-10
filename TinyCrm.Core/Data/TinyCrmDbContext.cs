52f1d4df2c9cffc9545667af07504ea36f7107d4
konsta-katsanou <58183830+konsta-katsanou@users.noreply.github.com>
2020-02-10 11:50:28 +0200

    initial commit

---
+++ TinyCrm.Core/Data/TinyCrmDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace TinyCrm.Core.Data
+{
+    public class TinyCrmDbContext : DbContext
+    {
+        private readonly string connectionString_;
+
+        public TinyCrmDbContext(string connString)
+        {
+            connectionString_ = connString;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder
+                .Entity<Model.Customer>()
+                .ToTable("Customer", "core");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(connectionString_);
+        }
+    }
+}
